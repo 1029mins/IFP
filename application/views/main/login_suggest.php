186f394e8c761b8040309170d0b061f9df30feab
zellytozelly <zellytozelly@gmail.com>
2020-08-30 20:20:10 +0900

    FEAT login 권한 필요 알림 page

---
+++ application/views/main/login_suggest.php
@@ -0,0 +1,28 @@
+<style>
+.nopage {
+    margin: 0px auto;
+    padding: 4%;
+    font-size: 1.2em;
+    background: #fff;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    text-align: center;
+}
+</style>
+
+
+</section>
+<section id="main">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+                <div class="nopage">
+                    <h1>
+                        로그인 후 사용 가능 합니다.				
+                    </h1>
+                </div>
+                <br>
+            </div>
+		</div>
+	</div>
+</section>
